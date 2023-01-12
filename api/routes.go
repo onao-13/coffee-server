57d6f89f2680800f80488b6be3cfc19e1f30f0af
onao13 <khovratovic15@yandex.ru>
2023-01-12 10:31:53 +0300

    Update route api

--- api/routes.go
+++ api/routes.go
@@ -8,9 +8,9 @@ import (
 
 func CreateRoutes(coffeeController *controller.CoffeeController) *mux.Router {
 	route := mux.NewRouter()
-	route.HandleFunc("/coffee/{id:[0-9]+}", coffeeController.FindById).Methods("GET")
-	route.HandleFunc("/coffee/add-new", coffeeController.AddNew).Methods("POST")
-	route.HandleFunc("/coffee/get-all", coffeeController.GetAll).Methods("GET")
+	route.HandleFunc("/api/v1/coffee/{id:[0-9]+}", coffeeController.FindById).Methods("GET")
+	route.HandleFunc("/api/v1/coffee/add-new", coffeeController.AddNew).Methods("POST")
+	route.HandleFunc("/api/v1/coffee/get-all", coffeeController.GetAll).Methods("GET")
 	route.NotFoundHandler = route.NewRoute().HandlerFunc(controller.NotFound).GetHandler()
 	return route
 }
